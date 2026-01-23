32f33170638e92c20770df41e7ef2493ba33c43b
Fernando Oriundo <faoriundo@gmail.com>
2026-01-22 19:52:32 -0500

    hw1

---
+++ Lab1/lab1.mjs
@@ -0,0 +1,36 @@
+export const functionOne = (arr) => {
+  // Implement function 1 here
+  for (let x of arr) {
+    let counter = 0;
+    let temp = 0
+    // console.log(x);
+    for (let j of x) {
+    
+      console.log(j);
+
+  }
+}
+  return ; //return result
+};
+
+export const functionTwo = (arr) => {
+  // Implement function 2 here
+  return; //return result
+};
+
+export const functionThree = (str) => {
+  // Implement function 3 here
+  return; //return result
+};
+
+export const functionFour = (arr) => {
+  // Implement function 4 here
+  return; //return result
+};
+
+//DO NOT FORGET TO UPDATE THE INFORMATION BELOW OR IT WILL BE -2 POINTS PER FIELD THAT IS MISSING.
+export const studentInfo = {
+  firstName: 'Fernando',
+  lastName: 'Oriundo',
+  studentId: '20027485'
+};
---
+++ Lab1/lab1.test.mjs
@@ -0,0 +1,4 @@
+import * as lab1 from './lab1.mjs';
+
+//TODO: Write and call each function in lab1.js 5 times each, passing in different input
+console.log(lab1.functionOne([987, 1234, -49])); // returns and then outputs: [30, 11, 17]
