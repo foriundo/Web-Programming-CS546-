3566007bd885ea064d791ed0f050682f65d899e1
Fernando Oriundo <faoriundo@gmail.com>
2026-01-25 16:18:15 -0500

    homework day1

--- Lab1/lab1.mjs
+++ Lab1/lab1.mjs
@@ -1,21 +1,63 @@
 export const functionOne = (arr) => {
   // Implement function 1 here
-  for (let x of arr) {
-    let counter = 0;
-    let temp = 0
-    // console.log(x);
-    for (let j of x) {
+  let res = []
+  for  (let i of arr){
+    let first_sum = 0;
     
-      console.log(j);
+    for ( let j of String(Math.abs(i))){
+      first_sum += Number(j);
+    }
+
+    let temp = 0;
+    let final_sum = first_sum;
+
+    while (first_sum > 9){
+      for (let k of String(first_sum)){
+        temp += Number(k)
+      }
+      first_sum = temp;
+      final_sum += temp;
+      temp = 0;
+    }
+    
+    res.push(final_sum);
 
   }
-}
-  return ; //return result
+  return res; //return result
 };
 
 export const functionTwo = (arr) => {
   // Implement function 2 here
-  return; //return result
+
+  if(arr.length === 0){
+    return {}
+  }
+
+  let res = {}
+
+  for (let i of arr){
+    let word = i.toLowerCase()
+    let counter = 0;
+
+    let mySet = new Set(word);
+    let arr_word = [...mySet];
+    // console.log(arr_word);
+
+    for (let j of arr_word){
+
+      if( /^[A-Za-z]$/.test(j)){
+      
+        if ( j !== 'a' && j !== 'e' && j !== 'i' && j !== 'o' && j !== 'u'){
+          counter ++;
+      }
+    }
+    }
+    res[word] = counter;
+    counter = 0;
+
+  }
+
+  return res; //return result
 };
 
 export const functionThree = (str) => {
--- Lab1/lab1.test.mjs
+++ Lab1/lab1.test.mjs
@@ -1,4 +1,15 @@
 import * as lab1 from './lab1.mjs';
 
 //TODO: Write and call each function in lab1.js 5 times each, passing in different input
-console.log(lab1.functionOne([987, 1234, -49])); // returns and then outputs: [30, 11, 17]
+//FunctionOne
+// console.log(lab1.functionOne([987, 1234, -49])); // returns and then outputs: [30, 11, 17]
+// console.log(lab1.functionOne([5, 10, 99])); // returns and then outputs: [5, 1, 27] 
+// console.log(lab1.functionOne([5])); // returns and then outputs: [0]
+// console.log(lab1.functionOne([19, 29, 199])); // returns and then outputs: [11, 13, 30]
+// console.log(lab1.functionOne([-9999, 111, 808])); // returns and then outputs: [45, 3, 23]
+
+console.log(lab1.functionTwo(["hello", "world", "AEIOU"])); // returns and then outputs: {hello: 2, world: 4, AEIOU: 0} 
+console.log(lab1.functionTwo(["banana", "Programming", "sky"])); // returns and then outputs: {banana: 2, Programming: 5, sky: 3} 
+console.log(lab1.functionTwo(["xyz"])); // returns and then outputs:  {xyz: 3}
+console.log(lab1.functionTwo([])); // returns and then outputs: {}
+console.log(lab1.functionTwo(["B2B!!!", "Shh...", "rhythm"])); // returns and then outputs: {B2B!!!: 1, Shh...: 2, rhythm: 5}
