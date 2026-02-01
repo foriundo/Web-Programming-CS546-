82755f323a654c98c35c6c1a1b2b04d6404f65b0
Fernando Oriundo <faoriundo@gmail.com>
2026-02-01 16:36:07 -0500

    finish assignement

--- Lab1/lab1.mjs
+++ Lab1/lab1.mjs
@@ -1,27 +1,26 @@
 export const functionOne = (arr) => {
   // Implement function 1 here
-  let res = []
-  for  (let i of arr){
+  let res = [];
+  for (let i of arr) {
     let first_sum = 0;
-    
-    for ( let j of String(Math.abs(i))){
+
+    for (let j of String(Math.abs(i))) {
       first_sum += Number(j);
     }
 
     let temp = 0;
     let final_sum = first_sum;
 
-    while (first_sum > 9){
-      for (let k of String(first_sum)){
-        temp += Number(k)
+    while (first_sum > 9) {
+      for (let k of String(first_sum)) {
+        temp += Number(k);
       }
       first_sum = temp;
       final_sum += temp;
       temp = 0;
     }
-    
-    res.push(final_sum);
 
+    res.push(final_sum);
   }
   return res; //return result
 };
@@ -29,32 +28,28 @@ export const functionOne = (arr) => {
 export const functionTwo = (arr) => {
   // Implement function 2 here
 
-  if(arr.length === 0){
-    return {}
+  if (arr.length === 0) {
+    return {};
   }
 
-  let res = {}
+  let res = {};
 
-  for (let i of arr){
-    let word = i.toLowerCase()
+  for (let i of arr) {
+    let word = i.toLowerCase();
     let counter = 0;
 
     let mySet = new Set(word);
     let arr_word = [...mySet];
-    // console.log(arr_word);
 
-    for (let j of arr_word){
-
-      if( /^[A-Za-z]$/.test(j)){
-      
-        if ( j !== 'a' && j !== 'e' && j !== 'i' && j !== 'o' && j !== 'u'){
-          counter ++;
+    for (let j of arr_word) {
+      if (/^[a-z]$/.test(j)) {
+        if (j !== "a" && j !== "e" && j !== "i" && j !== "o" && j !== "u") {
+          counter++;
+        }
       }
-    }
     }
     res[word] = counter;
     counter = 0;
-
   }
 
   return res; //return result
@@ -62,27 +57,30 @@ export const functionTwo = (arr) => {
 
 export const functionThree = (str) => {
   // Implement function 3 here
-  let obj =  {mostCommonLength: 0, words: null, averageLength: 0}
-  if(str.length === 0){
+  let obj = { mostCommonLength: 0, words: null, averageLength: 0 };
+  if (str.length === 0) {
     return obj;
   }
   let new_arr = str.split(" ");
   let avg = 0;
   let commonLength = {};
 
-  for(let x of new_arr){
+  for (let x of new_arr) {
     avg += x.length;
     commonLength[x.length] = (commonLength[x.length] || 0) + 1;
   }
-  obj["averageLength"] = (Math.floor(avg / (new_arr.length)));
-  
+  obj["averageLength"] = Math.floor(avg / new_arr.length);
+
   let highestCount = 0;
   let mostCommon = Infinity;
 
   for (let len in commonLength) {
     let count = commonLength[len];
     let numLen = Number(len);
-    if( count > highestCount || (count === highestCount && numLen < mostCommon)){
+    if (
+      count > highestCount ||
+      (count === highestCount && numLen < mostCommon)
+    ) {
       highestCount = count;
       mostCommon = numLen;
     }
@@ -91,8 +89,8 @@ export const functionThree = (str) => {
   obj["mostCommonLength"] = mostCommon;
 
   let wordsArr = [];
-  for(let x of new_arr){
-    if(x.length === mostCommon){
+  for (let x of new_arr) {
+    if (x.length === mostCommon) {
       wordsArr.push(x);
     }
   }
@@ -103,28 +101,73 @@ export const functionThree = (str) => {
 };
 
 export const functionFour = (arr) => {
+  // Implement function 4 here  
+  let strSet = new Set();
+  let numSet = new Set();
 
-  let mySet = new Set(arr);
-  let newArr = [...mySet];
-
-  // Implement function 4 here
-  let numArr = new Set();
-  let alphaArr = new Set();
-  for(let x of arr){
-    if(Number.isInteger(x)){
-      numArr.add(x);
-    }
-    else{
-      alphaArr.add(x);
+  for (let x of arr) {
+    if (typeof x === "number") {
+      numSet.add(x);
+    } else if (typeof x === "string") {
+      strSet.add(x);
     }
   }
-  console.log(alphaArr);
-  return; //return result
+
+  let newStr = [...strSet];
+  let newNum = [...numSet];
+
+  let vowelCount = (s) => {
+    let count = 0;
+    let lower = s.toLowerCase();
+
+    for (let ch of lower) {
+      if (ch === "a" || ch === "e" || ch === "i" || ch === "o" || ch === "u") {
+        count++;
+      }
+    }
+    return count;
+  };
+
+  newStr.sort((a, b) => {
+    let va = vowelCount(a);
+    let vb = vowelCount(b);
+
+    if (va < vb) {
+      return -1;
+    } else if (va > vb) {
+      return 1;
+    } else {
+
+      if (a < b) {
+        return -1;
+      } else if (a > b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  });
+
+  let evens = [];
+  let odds = [];
+
+  for (let n of newNum) {
+    if (n % 2 === 0) {
+      evens.push(n);
+    } else {
+      odds.push(n);
+    }
+  }
+
+  evens.sort((a, b) => a - b);
+  odds.sort((a, b) => a - b);
+
+  return [...newStr, ...evens, ...odds]; //return result
 };
 
 //DO NOT FORGET TO UPDATE THE INFORMATION BELOW OR IT WILL BE -2 POINTS PER FIELD THAT IS MISSING.
 export const studentInfo = {
-  firstName: 'Fernando',
-  lastName: 'Oriundo',
-  studentId: '20027485'
+  firstName: "Fernando",
+  lastName: "Oriundo",
+  studentId: "20027485",
 };
--- Lab1/lab1.test.mjs
+++ Lab1/lab1.test.mjs
@@ -2,25 +2,25 @@ import * as lab1 from './lab1.mjs';
 
 //TODO: Write and call each function in lab1.js 5 times each, passing in different input
 //FunctionOne
-// console.log(lab1.functionOne([987, 1234, -49])); // returns and then outputs: [30, 11, 17]
-// console.log(lab1.functionOne([5, 10, 99])); // returns and then outputs: [5, 1, 27] 
-// console.log(lab1.functionOne([5])); // returns and then outputs: [0]
-// console.log(lab1.functionOne([19, 29, 199])); // returns and then outputs: [11, 13, 30]
-// console.log(lab1.functionOne([-9999, 111, 808])); // returns and then outputs: [45, 3, 23]
+console.log(lab1.functionOne([987, 1234, -49])); // returns and then outputs: [30, 11, 17]
+console.log(lab1.functionOne([5, 10, 99])); // returns and then outputs: [5, 1, 27] 
+console.log(lab1.functionOne([5])); // returns and then outputs: [0]
+console.log(lab1.functionOne([19, 29, 199])); // returns and then outputs: [11, 13, 30]
+console.log(lab1.functionOne([-9999, 111, 808])); // returns and then outputs: [45, 3, 23]
 
 //FunctionTwo
-// console.log(lab1.functionTwo(["hello", "world", "AEIOU"])); // returns and then outputs: {hello: 2, world: 4, AEIOU: 0} 
-// console.log(lab1.functionTwo(["banana", "Programming", "sky"])); // returns and then outputs: {banana: 2, Programming: 5, sky: 3} 
-// console.log(lab1.functionTwo(["xyz"])); // returns and then outputs:  {xyz: 3}
-// console.log(lab1.functionTwo([])); // returns and then outputs: {}
-// console.log(lab1.functionTwo(["B2B!!!", "Shh...", "rhythm"])); // returns and then outputs: {B2B!!!: 1, Shh...: 2, rhythm: 5}
+console.log(lab1.functionTwo(["hello", "world", "AEIOU"])); // returns and then outputs: {hello: 2, world: 4, AEIOU: 0} 
+console.log(lab1.functionTwo(["banana", "Programming", "sky"])); // returns and then outputs: {banana: 2, Programming: 5, sky: 3} 
+console.log(lab1.functionTwo(["xyz"])); // returns and then outputs:  {xyz: 3}
+console.log(lab1.functionTwo([])); // returns and then outputs: {}
+console.log(lab1.functionTwo(["B2B!!!", "Shh...", "rhythm"])); // returns and then outputs: {B2B!!!: 1, Shh...: 2, rhythm: 5}
 
 //FunctionoThree
-// console.log(lab1.functionThree("Hello world from Stevens")); // returns and then outputs: {mostCommonLength: 5, words: "Hello, world", averageLength: 5}
-// console.log(lab1.functionThree("This is a test.")); // returns and then outputs: {mostCommonLength: 1, words: "a", averageLength: 3}
-// console.log(lab1.functionThree("OneWord")); // returns and then outputs: {mostCommonLength: 7, words: "OneWord", averageLength: 7}
-// console.log(lab1.functionThree("")); // returns and then outputs: {mostCommonLength: 0, words: null, averageLength: 0}
-// console.log(lab1.functionThree("I am programming now! now!")); // returns and then outputs: {mostCommonLength: 4, words: "now!, now!", averageLength: 4}
+console.log(lab1.functionThree("Hello world from Stevens")); // returns and then outputs: {mostCommonLength: 5, words: "Hello, world", averageLength: 5}
+console.log(lab1.functionThree("This is a test.")); // returns and then outputs: {mostCommonLength: 1, words: "a", averageLength: 3}
+console.log(lab1.functionThree("OneWord")); // returns and then outputs: {mostCommonLength: 7, words: "OneWord", averageLength: 7}
+console.log(lab1.functionThree("")); // returns and then outputs: {mostCommonLength: 0, words: null, averageLength: 0}
+console.log(lab1.functionThree("I am programming now! now!")); // returns and then outputs: {mostCommonLength: 4, words: "now!, now!", averageLength: 4}
 
 //FunctionFour
 console.log(lab1.functionFour([3, "guitar", 1, "bass", -10, "bass", 3]));  // returns and then outputs: ["bass", "guitar", -10, 1, 3]
